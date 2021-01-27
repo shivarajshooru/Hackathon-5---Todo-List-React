bfa40b2d508ee3d22da20430fa8ebdf743fa83a5
shivaraj shooru <shivarajshooru@gmail.com>
2021-01-27 20:41:51 +0530

    done2041

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,42 @@
-import React from "react";
+import React, {useState} from "react";
+import ListItem from "./ListItem";
 import "./../styles/App.css";
 
-function App() 
-{
+function App() {
+	const [items, setItems] = useState([]);
+	const [newitem, setnewItem] =  useState("");
+	const addItem = () => {
+		items.push(newitem);
+		setItems([...items]);
+		setnewItem("")
+	};
+	const newic = (evt) => {
+		setnewItem(evt.target.value);
+	};
+
+	const onEdit = (editvalue, itemidx) => {
+		items[itemidx] = editvalue;
+		setItems([...items]); 
+	}
+
+	const onDelete = (itemidx) => {
+		items.splice(itemidx, 1)
+		setItems([...items]); 
+	}
+
 	return (
 	<div id="main">
-	//Do not alter main div
-	//Please do not alter the functional component as tests depend on the type of component.
+	<textarea id="task" onChange={newic} placeholder="New Item" value={newitem}></textarea>
+	<buttton id="btn" onClick={addItem} disabled={newitem.trim().length === 0}>
+		AddItem
+		</buttton>
+	{items.map((item, idx) => (
+			<ListItem item={item} key={`${item}_${idx}`} 
+
+			idx={idx } onEdit={onEdit} onDelete={onDelete} />
+		))}
 	</div>
-	);
+	); 
 }
 
 
---
+++ src/components/ListItem.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import "./../styles/App.css";
+
+function ListItem(props) {
+    const [editedI, setedit] = useState(props.item);
+    const [editm, seteditm] = useState(false);
+    const editic = (evt) => {
+		setedit(evt.target.value);
+    };
+    const saveedit = () => {
+        props.onEdit(onEdit, props.idx);
+
+        seteditm(false);
+
+    }
+
+	return (
+        <div className="list">
+           
+            {editm ?
+            <>
+            <textarea className="editTask" onChange="" placeholder="Edit task" value={editedI} ></textarea>
+            <button className="saveTask" onClick={saveedit} disabled={editedI.trim().length === 0}>Save Task</button>
+            </>
+            : <> {props.item}
+            <button className="edit" onClick={() => seteditm(true)}>edit</button>
+            <button className="delete" onClick={() => props.onDelete(props.idx)}>delete</button> </> }
+        </div>
+	);
+}
+
+
+export default ListItem;
+  
